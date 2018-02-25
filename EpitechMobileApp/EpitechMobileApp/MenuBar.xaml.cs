e46741dd4a7d44ad212e520c5a12cc1897ac3851
Azheris <guilhem.amardeilh@epitech.eu>
2018-02-25 02:22:26 +0100

    ADD | Menu Bar tools base

---
+++ EpitechMobileApp/EpitechMobileApp/MenuBar.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EpitechMobileApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MenuBar : MasterDetailPage
+	{
+		public MenuBar ()
+		{
+			InitializeComponent ();
+		}
+
+        void Profil_on_Click(object sender, System.EventArgs e)
+        {
+            IsPresented = false;
+        }
+
+        void Modules_on_Click(object sender, System.EventArgs e)
+        {
+            IsPresented = false;
+        }
+
+        void Planning_on_Click(object sender, System.EventArgs e)
+        {
+            IsPresented = false;
+        }
+    }
+}
